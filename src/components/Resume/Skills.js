a98da5994ddb2039aca9e1f241a52a1a78742fd4
Dinis <113453780+dinis-a@users.noreply.github.com>
2024-09-17 18:54:55 +0300

    Update Skills.js

--- src/components/Resume/Skills.js
+++ src/components/Resume/Skills.js
@@ -68,15 +68,15 @@ const Skills = ({ skills, categories }) => {
         <p>
           Here is a mostly honest overview of my skills.
         </p>
-      </div>
-      <div className="skill-button-container">{getButtons()}</div>
-      <div className="skill-row-container">{getRows()}</div>
-        <h3>Languages</h3>
+        <h2>Languages</h2>
         <p>🇷🇺 Russian - Native speaker
            🇪🇸 Spanish - Proficient - Diploma of Spanish as a Foreign Language, Level C1 (DELE C1)
            🇬🇧 English - Advanced - B2/C1
            🇸🇦 Arabic - Elementary / Communicative - A2
         </p>
+      </div>
+      <div className="skill-button-container">{getButtons()}</div>
+      <div className="skill-row-container">{getRows()}</div>
     </div>
   );
 };
