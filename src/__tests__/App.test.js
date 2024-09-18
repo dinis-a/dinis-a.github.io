fee3f64dab35783f82704cac58307c4730bf2f0c
Dinis <akulovdn@yandex.ru>
2024-09-18 19:30:20 +0300

    Initial commit

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -41,7 +41,7 @@ describe('renders the app', () => {
   });
 
   it('should render the title', async () => {
-    expect(document.title).toBe("Dinis Akulov");
+    expect(document.title).toBe('Dinis Akulov');
   });
 
   // it('can navigate to /about', async () => {
