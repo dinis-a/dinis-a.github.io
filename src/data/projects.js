073ccaf9fea2f90301e4ebb7073148d2227515df
Dinis <113453780+dinis-a@users.noreply.github.com>
2024-11-20 18:46:48 +0300

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,7 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'VoiceAI - dinisakulov.online',
+    title: 'VoiceAI',
     subtitle: 'Web Application',
     image: '/images/projects/voice.png',
     date: '2024-12-01',
@@ -9,7 +9,7 @@ const data = [
       "VoiceAI is an advanced AI voice assistant powered by Google's LLM. It offers intuitive and conversational interactions, leveraging state-of-the-art natural language processing to understand and respond to user queries with high accuracy.",
   },
   {
-    title: 'AI Assistant Chatbot - @GeminiDBot',
+    title: 'AI Assistant Chatbot',
     subtitle: 'Telegram Bot',
     image: '/images/projects/gemini.jpg',
     date: '2023-12-01',
@@ -17,7 +17,7 @@ const data = [
       'The AI Assistant Chatbot is a Telegram-based chatbot that allows users to interact seamlessly with a powerful artificial intelligence developed by Google. The bot offers real-time responses to user queries, making it an ideal tool for various tasks such as answering questions, providing information, and even offering creative suggestions. The chatbot integrates with Google AI technologies, bringing the capabilities of advanced machine learning and natural language understanding directly to your fingertips through the familiar Telegram interface.',
   },
   {
-    title: 'Skin Cancer Detection via Photo - @Skin_ArtI_bot',
+    title: 'Skin Cancer Detection via Photo',
     subtitle: 'Telegram bot',
     image: '/images/projects/melanoma.jpg',
     date: '2020-01-20',
