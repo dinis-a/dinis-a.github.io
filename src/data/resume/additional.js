ac0f8c1d7200dc6fa428e1a2c46c60d44d8eb02e
Dinis <akulovdn@yandex.ru>
2025-11-09 15:37:22 +0300

    test

--- src/data/resume/additional.js
+++
@@ -1,7 +0,0 @@
-const additional = [
-  {
-    info: 'III degree diploma in scientific conference “Current issues of experimental and clinical medicine – 2010” (04/2010)',
-  },
-];
-
-export default additional;
